154efc7bbea74dba10fc04725c08bd325069d8c3
themaktravels <michelle.a.kim@gmail.com>
2012-12-01 15:55:16 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyApp::Application.config.secret_token = 'c4ac9991c0ce769820c882ed9a5691e68d0c08ee0f604af551d72f85fe3b3d9847df68fc055f7f6a1334bd2d2f40443fd1b051ff73c1439a0746c3e390490200'
